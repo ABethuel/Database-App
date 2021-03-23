da752ac2b5406131bc56453b56a330298f48e157
ABethuel <adrienbethuel@gmail.com>
2021-03-23 11:31:06 +0100

    'correct'

--- app/src/main/java/com/example/databaseapp/HelpActivity.java
+++ app/src/main/java/com/example/databaseapp/HelpActivity.java
@@ -8,10 +8,8 @@ import android.view.View;
 import android.widget.Button;
 
 public class HelpActivity extends AppCompatActivity {
-
     Button backToMainActivity;
     Button aboutButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
