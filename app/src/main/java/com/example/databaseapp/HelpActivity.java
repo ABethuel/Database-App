b15768e12b5456d4267244966725e243cd7224c9
ABethuel <adrienbethuel@gmail.com>
2021-03-23 11:36:53 +0100

    correct 2

--- app/src/main/java/com/example/databaseapp/HelpActivity.java
+++ app/src/main/java/com/example/databaseapp/HelpActivity.java
@@ -8,9 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class HelpActivity extends AppCompatActivity {
+
     Button backToMainActivity;
     Button aboutButton;
     @Override
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
