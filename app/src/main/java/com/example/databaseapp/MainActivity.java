ea6c263a5759e5f80a208c6ebfaa8c9ac3335ab7
ABethuel <adrienbethuel@gmail.com>
2021-03-28 22:21:41 +0200

    SearchActivity and background MainActivity

--- app/src/main/java/com/example/databaseapp/MainActivity.java
+++ app/src/main/java/com/example/databaseapp/MainActivity.java
@@ -20,97 +20,19 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    RecyclerView.LayoutManager layoutManager;
-    SearchInput input;
-
-    MaterialSearchBar materialSearchBar;
-    List<String> suggestList = new ArrayList<>();
-
-    Database database;
-
     Button helpButton;
     Button aboutButton;
+    Button searchButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // There we will init the View
-        recyclerView = (RecyclerView)findViewById(R.id.recycler_search);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setHasFixedSize(true);
-
-        materialSearchBar = (MaterialSearchBar)findViewById(R.id.search_bar);
-
-        // Initialisation of the database
-        database = new Database(this );
-
-        // Search bar
-        materialSearchBar.setHint("Search");
-        materialSearchBar.setCardViewElevation(10);
-        loadSuggestList();
-        materialSearchBar.addTextChangeListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after)
-            {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count)
-            {
-                List<String> suggest = new ArrayList<>();
-                for (String search:suggestList)
-                {
-                    if (search.toLowerCase().contains(materialSearchBar.getText().toLowerCase()))
-                        suggest.add(search);
-                }
-                materialSearchBar.setLastSuggestions(suggest);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s)
-            {
-
-            }
-        });
-        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
-            @Override
-            public void onSearchStateChanged(boolean enabled)
-            {
-                if(!enabled)
-                {
-                    input = new SearchInput(getBaseContext(), database.getDrivers());
-                    recyclerView.setAdapter(input);
-                }
-
-            }
-
-            @Override
-            public void onSearchConfirmed(CharSequence text)
-            {
-                startSearch(text.toString());
-            }
-
-            @Override
-            public void onButtonClicked(int buttonCode) {
-
-            }
-        });
-
-        //Init first page (display all drivers)
-        input = new SearchInput(this, database.getDrivers());
-        recyclerView.setAdapter(input);
 
         setupControls();
     }
 
-    private void setupControls()
-    {
+    private void setupControls() {
         helpButton = findViewById(R.id.helpButton);
         helpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,17 +50,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
 
-    private void startSearch(String text)
-    {
-        input = new SearchInput(this, database.getDriverByName(text));
-        recyclerView.setAdapter(input);
-    }
-
-    private void loadSuggestList()
-    {
-        suggestList = database.getNames();
-        materialSearchBar.setLastSuggestions(suggestList);
+        searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/databaseapp/SearchActivity.java
@@ -0,0 +1,134 @@
+package com.example.databaseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.databaseapp.Database.Database;
+import com.example.databaseapp.Search.SearchInput;
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    SearchInput input;
+
+    MaterialSearchBar materialSearchBar;
+    List<String> suggestList = new ArrayList<>();
+
+    Database database;
+
+    Button backToMainActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        // There we will init the View
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_search);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        materialSearchBar = (MaterialSearchBar)findViewById(R.id.search_bar);
+
+        // Initialisation of the database
+        database = new Database(this );
+
+        // Search bar
+        materialSearchBar.setHint("Search");
+        materialSearchBar.setCardViewElevation(10);
+        loadSuggestList();
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                List<String> suggest = new ArrayList<>();
+                for (String search:suggestList)
+                {
+                    if (search.toLowerCase().contains(materialSearchBar.getText().toLowerCase()))
+                        suggest.add(search);
+                }
+                materialSearchBar.setLastSuggestions(suggest);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+
+            }
+        });
+        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled)
+            {
+                if(!enabled)
+                {
+                    input = new SearchInput(getBaseContext(), database.getDrivers());
+                    recyclerView.setAdapter(input);
+                }
+
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text)
+            {
+                startSearch(text.toString());
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
+
+        //Init first page (display all drivers)
+        input = new SearchInput(this, database.getDrivers());
+        recyclerView.setAdapter(input);
+
+        setupControls();
+
+    }
+
+    private void setupControls() {
+        backToMainActivity = findViewById(R.id.backToMainActivityButton);
+        backToMainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
+    }
+
+    private void startSearch(String text)
+    {
+        input = new SearchInput(this, database.getDriverByName(text));
+        recyclerView.setAdapter(input);
+    }
+
+    private void loadSuggestList()
+    {
+        suggestList = database.getNames();
+        materialSearchBar.setLastSuggestions(suggestList);
+    }
+}
